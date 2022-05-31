9dca484fe2d6631217e921b3259ae4ca243218ea
Fábio Santos <me@fabiofdsantos.com>
2022-05-31 14:27:49 +0100

    fix: force variant via url query param

--- lib/plugin.ts
+++ lib/plugin.ts
@@ -41,22 +41,22 @@ export function experimentVariant(
     return 0;
   }
 
-  const key = `${COOKIE_PREFIX}_${experimentName}`;
+  const cookieKey = `${COOKIE_PREFIX}_${experimentName}`;
 
   // Force a specific variant by url or param
-  const forceVariantByUrl = window.$nuxt.$route.query[key] as
+  const forceVariantByUrl = window.$nuxt.$route.query[experimentName] as
     | string
     | undefined;
 
   const variant = forceVariantByUrl ?? forceVariant?.toString() ?? undefined;
   if (variant) {
-    Cookies.set(key, variant, {
+    Cookies.set(cookieKey, variant, {
       expires: experiment.maxAgeDays,
     });
   }
 
   // Determine the active variant of the experiment
-  let activeVariant: string = Cookies.get(key) || "";
+  let activeVariant: string = Cookies.get(cookieKey) || "";
 
   if (activeVariant.length === 0) {
     const weights: number[] = experiment.variants.map((weight) =>
@@ -68,7 +68,7 @@ export function experimentVariant(
       activeVariant = weightedRandom(weights);
     }
 
-    Cookies.set(key, activeVariant, {
+    Cookies.set(cookieKey, activeVariant, {
       expires: experiment.maxAgeDays,
     });
   }
